3141990ef65921cb53ec036900774ef452a300ea
Rabie Nasser <rabie.nasser@tibercreek.com>
2022-03-29 09:36:38 -0400

    initial commit

---
+++ data/comments.js
@@ -0,0 +1,14 @@
+export const comments = [
+   {
+      id: 1,
+      text: "Text for comment number 1",
+   },
+   {
+      id: 2,
+      text: "Text for comment number 2",
+   },
+   {
+      id: 3,
+      text: "Text for comment number 3",
+   },
+];
---
+++ pages/comments/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function CommentsPage() {
+   const [comments, setComments] = useState([]);
+   const [comment, setComment] = useState("");
+
+   const fetchComments = async () => {
+      const res = await fetch("/api/comments");
+      const data = await res.json();
+      setComments(data);
+   };
+
+   const submitComment = async () => {
+      setComment("");
+      const res = await fetch("/api/comments", {
+         method: "POST",
+         body: JSON.stringify({ comment }),
+         headers: {
+            "Content-Type": "application/json",
+         },
+      });
+      const data = await res.json();
+      console.log(data);
+   };
+
+   return (
+      <>
+         <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+         />
+         <button onClick={submitComment}>Submit Comment</button>
+         <button onClick={fetchComments}>Load Comments</button>
+         {comments?.map((comment) => {
+            return (
+               <div key={comment.id}>
+                  {comment.id} {comment.text}
+               </div>
+            );
+         })}
+      </>
+   );
+}
+
+export default CommentsPage;
